724937bd7cc021c1064be7b490c9a291db1315c8
github username <github email address>
2019-04-10 12:36:03 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,10 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+    #raise Song.all.first.name.inspect
+  end
+
   def new
     @song = Song.new
 
@@ -6,8 +12,7 @@ class SongsController < ApplicationController
 
   def create
       #raise params.inspect
-      @song = Song.new
-      @song.name = params[:song][:name]
+      @song = Song.new(song_params)
       @song.save
       redirect_to song_path(@song)
   end
@@ -19,5 +24,22 @@ class SongsController < ApplicationController
     #puts @song.name
   end
 
+  def edit
+    @song = Song.find(params[:id])
+    #raise params.inspect
+
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(name: params[:song][:name], artist_id: params[:song][:artist_id], genre_id: params[:song][:genre_id])
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :genre_id, :artist_id)
+  end
 
 end
